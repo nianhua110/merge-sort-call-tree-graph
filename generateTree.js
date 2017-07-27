50de946c6460c780d47e167fc942d999b97fd161
kyle <nianhua110@qq.com>
2017-07-27 15:21:52 +0800

    创建归并排序递归调用图

---
+++ generateTree.js
@@ -0,0 +1,90 @@
+/**
+ * Created by kyle on 17-7-27.
+ */
+export default function generateTree(a) {
+  let count = 0;
+  let tree = {name: '开始', children: [{}]};
+
+  function merge(a, start, mid, end) {
+    let b = [];
+    let i = start;
+    let j = mid + 1;
+    let m = mid;
+    let n = end;
+    while (i <= m && j <= n) {
+      if (a[i] > a[j]) {
+        b.push(a[j])
+        j++;
+      } else {
+        b.push(a[i])
+        i++;
+      }
+    }
+
+    while (i <= m) {
+      b.push(a[i]);
+      i++;
+    }
+    while (j < n) {
+      b.push(a[j]);
+      j++;
+    }
+
+    for (let i = 0; i < b.length; i++) {
+      a[start + i] = b[i];
+    }
+  }
+
+  function sort(a, start, end, currentNdoe) {
+    count = count + 1;
+
+    /*判断该节点是父节点的左节点还是右节点*/
+    if (currentNdoe && currentNdoe.parent && currentNdoe.parent.whichSideWhenChildNode) {
+      if (currentNdoe.parent.whichSideWhenChildNode === 'left') {
+        currentNdoe.parent.attributes.step3 = '执行sort' + count
+      } else {
+        currentNdoe.parent.attributes.step4 = '执行sort' + count;
+      }
+    }
+
+    const name = 'sort' + count + '( a, ' + start + ', ' + end + ' )'
+    console.log(name);
+    currentNdoe.name = name;
+    currentNdoe.attributes = {}
+
+    /*step1, 判断递归结束*/
+    if (start >= end) {
+      console.log('1, 返回');
+      currentNdoe.attributes.step1 = '返回';
+      return;
+    }
+    console.log('1, 跳过')
+    currentNdoe.attributes.step1 = '跳过';
+
+    /*step２, 计算中位数*/
+    const mid = Math.floor((start + end) / 2)
+    console.log('2, mid =' + mid);
+    currentNdoe.attributes.step2 = 'mid=' + mid;
+
+    currentNdoe.children = [
+      {parent: currentNdoe, whichSideWhenChildNode: 'left'},
+      {parent: currentNdoe, whichSideWhenChildNode: 'right'}
+    ]
+    sort(a, start, mid, currentNdoe.children[0]);
+    sort(a, mid + 1, end, currentNdoe.children[1]);
+    currentNdoe.attributes.step5 = '执行merge(a, ' + ', ' + start + ', '
+      + ', ' + mid + ', ' + end + ')'
+    merge(a, start,mid, end);
+    currentNdoe.attributes.end='当前a=['+a.reduce((r,v)=>(v+', '+r))+']';
+  }
+
+
+  tree.attributes = {array: ''}
+  for (let i = 0; i < a.length; i++) {
+    tree.attributes.array += a[i] + ', ';
+  }
+
+  sort(a, 0, a.length - 1, tree.children[0])
+  console.log(tree);
+  return tree
+}
---
+++ index.js
@@ -0,0 +1,71 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Tree from "react-d3-tree";
+import {Row, Col, Input, Button, message} from "antd";
+import "./index.css";
+import generateTree from "./generateTree";
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onChange = this.onChange.bind(this)
+    const array = [8, 2, 12, 6, 9, 7, 3, 88, 23];
+    const beforeSortArray = [...array];
+    const tree = generateTree(array);
+    const afterSortArray = [...array];
+    console.log('before', beforeSortArray);
+    console.log('after', afterSortArray);
+    console.log(JSON.stringify(afterSortArray));
+    const val = JSON.stringify(beforeSortArray)
+    this.state = {beforeSortArray, afterSortArray, inputVal: val.substring(1,val.length-1), tree}
+  }
+
+  onChange(e) {
+    const val = this.state.inputVal;
+    console.log(val);
+    if(/^(\d+,\s*)+\d+$/.test(val)){
+      const array = JSON.parse('['+val+']');
+      const beforeSortArray = [...array];
+      const tree = generateTree(array);
+      const afterSortArray = [...array];
+      console.log('before', beforeSortArray);
+      console.log('after', afterSortArray);
+      console.log(JSON.stringify(afterSortArray));
+      this.setState({ beforeSortArray, afterSortArray, tree })
+    }else {
+      message.error('输入的数据格式错误')
+    }
+  }
+
+  render() {
+
+    const tree = this.state.tree;
+    return (
+      <div>
+        <Row type="flex" style={{width: '2596px', height: '1024px'}}>
+          <Col span={2}>
+            请输入数组:
+            <Input defaultValue={this.state.inputVal}
+                   onChange={(e) => {
+                     const val = e.target.value;
+                     console.log(val);
+                     this.setState({inputVal: val})
+                   }}
+            />
+            <br/><br/>
+            <Button type="primary" onClick={this.onChange}>归并排序</Button>
+            <br/><br/>
+            结果:{this.state.afterSortArray.reduce((v, r) => (v + ', ' + r))}
+          </Col>
+          <Col>
+            <div style={{width: '2096px', height: '1024px', border: 'solid 1px black', fontSize: '20px'}}>
+              <Tree data={[tree]} nodeSize={ {x: 300, y: 200}} translate={ {x: 50, y: 500}}></Tree>
+            </div>
+          </Col>
+        </Row>
+
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
